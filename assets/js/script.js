29649284d22422db825534fde0d3c8c501680e32
Abdullah <m.abdullahakram01@gmail.com>
2025-10-06 12:18:47 +0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+document.querySelector('video').playbackRate = 2.0;
+
+
+// let pad = document.getElementsByClassName("intro-con");
+// let nav = document.getElementsByClassName("navbar-toggler");
+// // console.log(nav);
+// nav[0].addEventListener("click", function(){
+//     console.log(pad[0].style.paddingTop);
+//     if(pad[0].style.paddingTop != ''){
+//         pad[0].style.paddingTop = '';
+//     }else{
+//         pad[0].style.paddingTop = '200px';
+//     }
+
+// });
